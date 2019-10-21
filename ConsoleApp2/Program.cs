77dfad51fdc2b80d3288cc7957a334ade51a020d
Алексей Ю. Тюрин <aturin@mavt.lcl>
2019-10-21 12:33:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(4);
+        }
+    }
+    class Account
+    {
+        public int Id { get; private set; } // номер счета
+        public Account(int _id)
+        {
+            Id = _id;
+        }
+    }
+}
